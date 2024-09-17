d0b75362b8b92d3cdc41c67df2afe496c01e2145
Simion Petrica <95485582+Petrica81@users.noreply.github.com>
2024-09-17 22:52:16 +0300

    Grid movement (#15)
    * nothinh
    
    * grid movement
    
    * speed fix

---
+++ Assets/Scripts/NPC/NPCGridMovement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCGridMovement : MonoBehaviour
+{
+    [SerializeField] List<Transform> _targets;
+    NPCPathfinding _pathfinding;
+    private NPC _npc;
+    int _iterator = 0;
+    public Vector2 secondsToStopInterval;
+    private bool isStopping = false;
+
+    private int numberOfTargets = 0;
+    private bool _AfterPause = false;
+    Rigidbody2D _rigidbody;
+    void Start()
+    {
+        _pathfinding = GetComponent<NPCPathfinding>();
+        _npc = GetComponent<NPC>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        numberOfTargets = _targets.Count;
+    }
+
+    void Update()
+    {
+        if (PauseManager.Instance.isPaused || _npc.possessed)
+        {
+            _rigidbody.velocity = Vector2.zero;
+            _pathfinding.StopAllCoroutines();
+            _AfterPause = true;
+        }
+        else if (_AfterPause)
+        {
+            _AfterPause = false;
+            if (Vector2.Distance(_targets[_iterator].position, transform.position) < 0.5)
+            {
+                _pathfinding._end = null;
+            }
+            else
+            {
+                _pathfinding.CalculatePath(_targets[_iterator]);
+            }
+        }
+        if (numberOfTargets > 0 && !isStopping && _pathfinding._end == null) {
+            StartCoroutine(StopForRandomTime());
+        }
+    }
+
+    public void SetRandomDestination()
+    { 
+        int newIterator = Random.Range(0, numberOfTargets);
+
+        while(newIterator == _iterator)
+            newIterator = Random.Range(0, numberOfTargets);
+
+        _iterator = newIterator;
+
+    }
+
+    IEnumerator StopForRandomTime()
+    {
+        isStopping = true;
+
+        float stopTime = Random.Range(secondsToStopInterval.x, secondsToStopInterval.y);
+        yield return new WaitForSeconds(stopTime);
+
+        SetRandomDestination();
+        isStopping = false;
+        _pathfinding.CalculatePath(_targets[_iterator]);
+    }
+}
---
+++ Assets/Scripts/NPC/Pathfinding/NPCPathfinding.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCPathfinding : MonoBehaviour
+{
+    private Pathfinding _pathfinding;
+
+    public float _speed = 1f;
+
+    private Queue<Tile> _path;
+
+    private Tile _start;
+    public Tile _end;
+    private Rigidbody2D _rigidbody;
+
+    private void Start()
+    {
+        _pathfinding = FindAnyObjectByType<Pathfinding>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    public void CalculatePath(Transform target)
+    {
+        _start = _pathfinding.GetStart(transform);
+        _end = _pathfinding.GetEnd(target);
+        Queue<Tile> path = _pathfinding.FindPath(_start, _end);
+        _start._Text = "Start";
+        _end._Text = "End";
+        Debug.Log("a fost calculat apth-ul");
+        SetPath(path);
+    }
+    private void SetPath(Queue<Tile> path)
+    {
+        _path = path;
+        StopAllCoroutines();
+        StartCoroutine(MoveAlongPath(path));
+    }
+
+    private IEnumerator MoveAlongPath(Queue<Tile> path)
+    {
+        yield return new WaitForSeconds(0.2f);
+        Vector3 offset = new Vector3(0f, 0.5f, 0f);
+        Vector3 lastPosition = transform.position;
+        Debug.Log("Incepe coroutina");
+        while (path.Count > 0)
+        {
+            Tile nextTile = path.Dequeue();
+            Vector3 nextPosition = nextTile.transform.position + offset;
+            
+            Debug.Log(nextTile);
+            _rigidbody.velocity = (nextPosition - lastPosition) * _speed;
+
+            yield return new WaitUntil(() => Vector2.Distance(transform.position, nextPosition) <= 0.2);
+            _rigidbody.velocity = Vector2.zero;
+            lastPosition = nextTile.transform.position + offset;
+        }
+        _end = null;
+    }
+
+}
